30fa17968b8492af5df1f5a49eb739351ef9c5bc
Simon Friis Vindum <simonfv@gmail.com>
2024-04-14 13:08:04 +0200

    Update projectdo to v0.2.1

--- projectdo.rb
+++ projectdo.rb
@@ -1,8 +1,8 @@
 class Projectdo < Formula
   desc "Context-aware single-letter project commands to speed up your terminal workflow"
   homepage "https://github.com/paldepind/projectdo"
-  url "https://github.com/paldepind/projectdo/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "fd3930a408199d4aa61ee6bfa0f2266a481fc933c634b69631f2766d07d0f92b"
+  url "https://github.com/paldepind/projectdo/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "329b8abf29e19dc7ab524277974d011e72383db2dace0c7baccf8b201a669b4a"
   license "GPL-3.0"
   head "https://github.com/paldepind/projectdo.git"
 
